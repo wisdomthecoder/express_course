d753f88112eb0864e3ef6bf1f67412b3a4df8db2
Wisdom Dauda <codewithwise@gmail.com>
2025-07-09 01:06:24 +0100

    PUT

--- src/index.mjs
+++ src/index.mjs
@@ -1,36 +1,37 @@
-import express from 'express';
+import express, { response } from 'express';
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-const mockUsers = [ 
+const mockUsers = [
     {
         id: 1,
         username: "wisdom",
-        displayName:"Wisdom"
+        displayName: "Wisdom"
     },
     {
         id: 2,
         username: "kwopnan ",
-        displayName:"Kwopnan"
+        displayName: "Kwopnan"
     },
     {
         id: 3,
         username: "manret",
-        displayName:"Manret"
+        displayName: "Manret"
     },
     {
         id: 4,
         username: "nanmet",
-        displayName:"Nanment"
+        displayName: "Nanment"
     },
-  
+
     {
         id: 5,
         username: "dang",
-        displayName:"Dang"
+        displayName: "Dang"
     },
-  
-    
+
+
 ];
 
 app.get("/", (req, res) => {
@@ -41,12 +42,20 @@ app.get("/", (req, res) => {
 app.get("/api/users", (req, res) => {
     console.log(req.query);
     const { filter, value } = req.query;
-    if(filter && value) return res.send(mockUsers.filter((user)=>user[filter].includes(value)))
+    if (filter && value) return res.send(mockUsers.filter((user) => user[filter].includes(value)))
     return res.send(mockUsers);
-
-
-
 });
+
+app.post("/api/users", (req, res) => {
+    console.log(req.body)
+    const { body } = req;
+    const newUser = {
+        id: mockUsers[mockUsers.length - 1].id + 1, ...body
+    }
+    mockUsers.push(newUser);
+    return res.status(201).send(newUser);
+})
+
 app.get("/api/users/:id", (req, res) => {
     console.log(req.params);
     const parsedId = parseInt(req.params.id);
@@ -61,6 +70,19 @@ app.get("/api/users/:id", (req, res) => {
     }
 
 });
+
+app.put("/api/users/:id", (req, res) => {
+    const { body, params: { id } } = req;
+    const parseId = parseInt(id);
+    if (isNaN(parseId)) return res.sendStatus(400);
+    
+    const findUserIndex = mockUsers.findIndex((user) => user.id == parseId);
+    if (findUserIndex == -1) return res.sendStatus(404);
+
+    mockUsers[findUserIndex] = { id: parseId, ...body };
+    return res.sendStatus(200);
+
+})
 
 
 app.listen(PORT, () => {
